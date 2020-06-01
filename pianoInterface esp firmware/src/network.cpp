4094cf8068e9328dcf2029a6d7f5d825bb1db912
Michael Manning <michaelman11@gmail.com>
2020-05-31 20:48:33 -0600

    Switched from sockets to webserver

--- pianoInterface esp firmware/src/network.cpp
+++ pianoInterface esp firmware/src/network.cpp
@@ -18,16 +18,10 @@
 namespace
 {
     unsigned long lastMessageMillis = 0;
-    bool connected = false; //message was received within the last 2 seconds
     byte messageBuffer[8];
     byte songStreamBuffer[music::maxNoteCount];
     char songConversionBuffer[music::maxNoteCount];
 
-    // these are for song loading
-    unsigned int expectedSongLength = 0;
-    unsigned int expectedNoteCount = 0;
-    // expecting the next message to be song data
-    bool expectingSong = false;
     unsigned int loaderFrameIndex;      // What frame of the song was last loaded
     byte frameNoteIndex;                // How many notes for the currently loading frame have been loaded
     byte currentFrameNotes[_PIANOSIZE]; // Storage for loading notes in the frame before being memcopied by ::music
@@ -44,30 +38,7 @@ namespace
         return combined;
     }
 
-    void ClientReply(const char * message){
-        client.println(message);
-    }
-
-    void formattedClientReply(char *fmt, ...)
-    {
-        char buff[128];
-        va_list va;
-        va_start(va, fmt);
-        vsprintf(buff, fmt, va);
-        va_end(va);
-        sprintf(buff, "%s", buff);
-        client.println(buff);
-    }
-
-    void disconnectClient(){
-        connected = false;
-        lights::setGreenLED(false);
-        client.flush();
-        client.stop();
-        debug::println("disconnected from client");
-    }
-
-    constexpr int intArg(const char * name){
+    int intArg(const char * name){
         return webServer.arg(name).toInt();
     }
 } // namespace
@@ -171,7 +142,7 @@ namespace network
     // Whether the network is currently connected
     bool isConnected()
     {
-        return connected;
+        return false;
     }
 
     void pollOTA()
@@ -188,6 +159,7 @@ namespace network
 
     void handleGetStatus()
     {
+        // formattedClientReply("Status: Error lock code: %d", static_cast<uint8_t>(getCurrentError()));
         webServer.send(200, "text/plane", "OK");
     }
 
@@ -216,8 +188,8 @@ namespace network
             return;
         }
 
-        expectedSongLength = intArg("frames");
-        expectedNoteCount = intArg("notes");
+        unsigned int expectedSongLength = intArg("frames");
+        unsigned int expectedNoteCount = intArg("notes");
 
         Serial.println("expecting " + String(expectedSongLength) + " frames and " + String(expectedNoteCount) + " notes");
 
@@ -235,37 +207,28 @@ namespace network
         uint16_t frameCount = 0, noteCount = 0;
         for (size_t i = 0; i < byteCount; i++)
         {
-           // Serial.printf("Data[%d]: %d", i, temp[i]);
-           // Serial.println("");
             if (temp[i] == 250U)
             {
                 music::loadFrame(temp + lastFrameIndex, frameLength);
                 lastFrameIndex = i + 1;
                 frameCount++;
                 frameLength = 0;
-              //  Serial.printf("Framecount: %d" , frameCount);
-              //  Serial.println("");
             }
             else
             {
-                // songStreamBuffer[i] -= MIDI::noteNumberOffset;
                 noteCount++;
                 frameLength++;
             }
         }
-     //   Serial.printf("Framecount: %d" , frameCount);
-      //  Serial.println("");
+
         if (expectedNoteCount != noteCount || expectedSongLength != frameCount)
         {
-         //   Serial.printf("ER: Unexpected song length. %d / %d frames, %d / %d notes", frameCount, expectedSongLength, noteCount, expectedNoteCount);
-           // Serial.println("");
-            webServer.send(200, "text/plain", "Failed");
+            webServer.send(400, "text/plain", "Failed");
             fatalError(ErrorCode::SONG_LOAD_UNEXPECTED_FRAME_COUNT);
             return;
         }
 
-        //debug::println("uploaded successfully");
-        webServer.send(200, "text/cool", "Upload ok");
+        webServer.send(200, "text/plain", "Upload ok");
 
         Event e;
         e.action = []() {
@@ -279,245 +242,64 @@ namespace network
         PushEvent(e);
 
         music::setLoopingSettings(true, 0, expectedSongLength - 1);
-        expectedSongLength = 0;
-        expectedNoteCount = 0;
     }
 
-
-
-
-
-
-
-
-
-
-
-    void changeSetting(byte messageBuffer[8])
+    void handleChangeSetting()
     {
-        if (messageBuffer[1] > 7)
+        int setting = intArg("setting");
+        if(setting > 7)
         {
+            webServer.send(400, "text/plane", "Invalid setting number");
             fatalError(ErrorCode::INVALID_SETTING);
-            ClientReply("ER:Invalid setting");
-            disconnectClient();
             return;
         }
-
-        settings::saveColorSetting(static_cast<unsigned int>(messageBuffer[1]), {messageBuffer[2], messageBuffer[3], messageBuffer[4]});
-        ClientReply("OK");
+        int argA = intArg("A");
+        int argB = intArg("B");
+        int argC = intArg("C");
+        settings::saveColorSetting(static_cast<unsigned int>(setting), {argA, argB, argC});
     }
 
-    void updateLoopSetting(byte messageBuffer[8])
+    void handleUpdateLoopSetting()
     {
-        bool enabled = messageBuffer[1];
-        uint16_t loopStart = concatBytes(messageBuffer[2], messageBuffer[3]);
-        uint16_t loopEnd = concatBytes(messageBuffer[4], messageBuffer[5]);
+        bool enabled = intArg("enabled");
+        int loopStart = intArg("start");
+        int loopEnd = intArg("end");
         music::setLoopingSettings(enabled, loopStart, loopEnd);
-        ClientReply("OK");
+        webServer.send(200, "text/plane", "OK");
     }
 
-    void beginSongLoading(byte messageBuffer[8])
-    {
-        //When a song is done loading, expected length is reset. If it is 0 here then something went wrong.
-        if (!assert_fatal(expectedSongLength == 0, ErrorCode::SONG_LOAD_DISCONTINUITY))
-        {
-            ClientReply("ER:Song length already set");
-            disconnectClient();
-            return;
-        }
-
-        expectedSongLength = concatBytes(messageBuffer[1], messageBuffer[2]);
-        expectedNoteCount = concatBytes(messageBuffer[3], messageBuffer[4]);
-
-        frameNoteIndex = 0;
-        loaderFrameIndex = 0;
-
-        if (!assert_fatal(expectedSongLength <= music::maxSongLength && expectedSongLength != 0, ErrorCode::SONG_LOAD_DISCONTINUITY))
-        {
-            formattedClientReply("ER:Song length out of range: %d", expectedSongLength);
-            disconnectClient();
-            return;
-        }
-
-        expectingSong = true;
-
-        // reply OK
-        formattedClientReply("Song header OK. Length: %d", expectedSongLength);
-
-        // update progress bar
-        music::resetSongLoader();
-        Event e;
-        e.action = []() {
-            lights::setAnimationMode(lights::AnimationMode::ProgressBar);
-            lights::AnimationParameters::setProgressBarValue(0.0f);
-        };
-        PushEvent(e);
-    }
-
-    void songData(byte messageBuffer[8])
-    {
-        uint16_t incommingFrameIndex = concatBytes(messageBuffer[1], messageBuffer[2]);
-
-        if (!assert_fatal(incommingFrameIndex <= expectedSongLength, ErrorCode::INVALID_SONG_FRAME_INDEX))
-        {
-            ClientReply("ER:Frame index out of expected range");
-            disconnectClient();
-            return;
-        }
-
-        // If there are more notes being pressed at once than there are notes on the piano, we have an issue.
-        if (!assert_fatal(frameNoteIndex <= 88, ErrorCode::SONG_LOAD_DISCONTINUITY))
-        {
-            ClientReply("ER: Invalid note index");
-            disconnectClient();
-            return;
-        }
-
-        if (incommingFrameIndex - loaderFrameIndex == 1)
-        {
-            // End of frame, so send what was loaded to the song loader
-            music::loadFrame(currentFrameNotes, frameNoteIndex);
-
-            // Expect the next frame and reset the note counter for the current frame
-            loaderFrameIndex++;
-            frameNoteIndex = 0;
-        }
-        else if (incommingFrameIndex - loaderFrameIndex != 0)
-        {
-            // ERROR: frame was skipped
-            fatalError(ErrorCode::SONG_LOAD_DISCONTINUITY);
-
-            formattedClientReply("ER:Frame skip detected. Last: %d, Recieved: %d", loaderFrameIndex, incommingFrameIndex);
-            disconnectClient();
-            return;
-        }
-
-        // read note values
-        for (size_t i = 3; i < 8; i++)
-        {
-            // 255 means no notes left in the message.
-            // This message contains the whole frame, or the end of a large frame
-            if (messageBuffer[i] == 255)
-            {
-                // Emtpy message. Should be impossible
-                if (i == 3)
-                {
-                    fatalError(ErrorCode::SONG_LOAD_DISCONTINUITY);
-                    disconnectClient();
-                    return;
-                }
-                break;
-            }
-            // Add note to the current frame
-            currentFrameNotes[frameNoteIndex] = messageBuffer[i] - MIDI::noteNumberOffset;
-            frameNoteIndex++;
-        }
-
-        // Reply with OK + debug info
-        //formattedClientReply("OK: %d notes loaded for frame %d", frameNoteIndex, loaderFrameIndex);
-        ClientReply("OK");
-
-        // Update the progress bar display
-        Event e;
-        e.action = []() {
-            float progress = (float)loaderFrameIndex / (float)expectedSongLength;
-            lights::AnimationParameters::setProgressBarValue(progress);
-        };
-        PushEvent(e);
-    }
-
-    void songEnding(byte messageBuffer[8])
-    {
-        if (loaderFrameIndex != expectedSongLength - 1)
-        {
-            formattedClientReply("ER:Expected %d notes, but only received %d", expectedSongLength - 1, loaderFrameIndex);
-            fatalError(ErrorCode::SONG_LOAD_DISCONTINUITY);
-        }
-        else
-        {
-            Event e;
-            e.action = []() {
-                lights::setAnimationMode(lights::AnimationMode::BlinkSuccess);
-                while (!lights::animationCompleted())
-                {
-                    lights::updateAnimation();
-                }
-                lights::setAnimationMode(lights::AnimationMode::Waiting);
-            };
-            PushEvent(e);
-
-            music::setLoopingSettings(true, 0, expectedSongLength - 1);
-            expectedSongLength = 0;
-            expectedNoteCount = 0;
-
-            ClientReply("OK: Song loaded");
-        }
-        expectingSong = false;
-    }
-
-    void setSongIndex(byte messageBuffer[8])
-    {
-        uint16_t newIndex = concatBytes(messageBuffer[1], messageBuffer[2]);
-        music::setFrame(newIndex);
-        //formattedClientReply("OK: index set to [%d]", music::currentFrameIndex());
-        ClientReply("OK");
-
-        Event e;
-        e.action = []() {
-            lights::forceRefresh();
-        };
-        PushEvent(e);
-    }
-
-    void getStatus(byte messageBuffer[8])
-    {
-        if (isErrorLocked())
-        {
-            formattedClientReply("Status: Error lock code: %d", static_cast<uint8_t>(getCurrentError()));
-        }
-        else
-        {
-            ClientReply("Status: OK");
-        }
-    }
-
-    void getCurrentFrameIndex()
-    {
-        formattedClientReply("OK:%d", music::currentFrameIndex());
-    }
-
-    void restoreSettings()
+    void handleRestoreSettings()
     {
         settings::restoreDefaults();
-        ClientReply("OK");
+        webServer.send(200, "text/plane", "OK");
     }
 
-    void getSetting(byte messageBuffer[8])
+    void handleGetSettings()
     {
-        if (messageBuffer[1] > 7)
-        {
-            fatalError(ErrorCode::INVALID_SETTING);
-            ClientReply("ER:Invalid setting");
-            disconnectClient();
-            return;
-        }
+        // if (messageBuffer[1] > 7)
+        // {
+        //     fatalError(ErrorCode::INVALID_SETTING);
+        //     ClientReply("ER:Invalid setting");
+        //     disconnectClient();
+        //     return;
+        // }
 
-        color col = settings::getColorSetting(static_cast<unsigned int>(messageBuffer[1]));
-        {
-            // formattedClientReply("OK:%c%c%c", col.r, col.b, col.b);
-            formattedClientReply("OK:%d,%d,%d", col.r, col.g, col.b);
-        }
+        // color col = settings::getColorSetting(static_cast<unsigned int>(messageBuffer[1]));
+        // {
+        //     // formattedClientReply("OK:%c%c%c", col.r, col.b, col.b);
+        //     formattedClientReply("OK:%d,%d,%d", col.r, col.g, col.b);
+        // }
     }
 
-    void setAnimationMode(byte messageBuffer[8])
+    void handleSetAnimationMode()
     {
-        if (messageBuffer[1] > 5)
+        int mode = intArg("mode");
+        if (mode > 5)
         {
-            formattedClientReply("ER: Invalid animation number: %d", messageBuffer[1]);
-            disconnectClient();
+            webServer.send(400, "text/plane", "Invalid mode");
             return;
         }
-        switch (messageBuffer[1])
+        switch (mode)
         {
         case 0:
             _pushModeSwitchEvent(lights::AnimationMode::None);
@@ -538,16 +320,15 @@ namespace network
             _pushModeSwitchEvent(lights::AnimationMode::Waiting);
             break;
         default:
-            formattedClientReply("ER:Internal %d", __LINE__);
             fatalError(ErrorCode::IMPOSSIBLE_INTERNAL);
             break;
         }
-        ClientReply("OK");
+        webServer.send(200, "text/plane", "OK");
     }
 
-    void saveSettings()
+    void handleSaveSettings()
     {
         settings::commitSettings();
-        ClientReply("OK");
+        webServer.send(200, "text/plane", "OK");
     }
 } // namespace network
